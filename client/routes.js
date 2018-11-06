891d2088c816c99271f8bcfcd61487e9b5d892fa
thiago ramos <thiagoramos@gmail.com>
2018-11-06 01:55:34 -0200

    W.I.P refact client to teravoz api

--- client/routes.js
+++ client/routes.js
@@ -1,30 +1,13 @@
 import express from "express";
 
-import {
-  addCustomerContact,
-  delegateCallApi,
-  lookupCustomerContact,
-} from "api/client";
+import { redirectCallToCallcenterAgent } from "api/client";
 
 const router = express.Router();
 
 /** Route receive Teravoz payloads, verify customer */
 router.post("/webhook", async (request, response) => {
-  const callData = request.body;
-  const { their_number: incomingCallNumber } = callData;
-  const customer = await lookupCustomerContact(incomingCallNumber);
-  const isNewCustomer = !customer.length;
-
-  if (callData.type === "call.standby") {
-    if (isNewCustomer) {
-      await addCustomerContact(callData);
-    }
-    await delegateCallApi(callData, isNewCustomer);
-  } else if (callData.type === "call.delegate") {
-    // console.log("webhook once => ", response.req.body);
-  }
-
-  response.send(callData);
+  await redirectCallToCallcenterAgent(request.body);
+  response.send(request.body);
 });
 
 router.get("/", (request, response) => {
